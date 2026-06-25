ae691ae2b1d10c2ed8e62ed096d5d293908228c5
shubhamusingh-commits <shubham.u.singh@kipi.ai>
2026-06-25 21:17:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,3 +48,4 @@ if INGRIDIENTS_LIST:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
